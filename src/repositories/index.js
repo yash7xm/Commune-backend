fbc9ee01c1324a4a662414040da368e857a17072
yash7xm <yash.7xm@gmail.com>
2024-03-10 15:37:23 +0530

    repositories added

---
+++ src/repositories/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  CrudRepository: require("./crud-repository"),
+  UserRepository: require("./user-repository"),
+  MembershipRepository: require("./membership-repository"),
+  ChannelRepository: require("./channel-repository"),
+  MessageRepository: require("./messages-repository"),
+};
