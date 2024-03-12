5b1b010292c5e2cc885ae88f73f929dfe52cd2bc
yash7xm <yash.7xm@gmail.com>
2024-03-12 15:07:35 +0530

    kafka consumer added

---
+++ src/kafka/consumer.js
@@ -0,0 +1,18 @@
+const kafka = require("./client");
+
+const consumer = kafka.consumer({ groupId: "chat-consumer-group" });
+const topic = "send-message";
+
+async function consumeMessages() {
+  await consumer.connect();
+  await consumer.subscribe({ topic, fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log(`Received message: ${message.value.toString()}`);
+      return message;
+    },
+  });
+}
+
+module.exports = consumeMessages;
